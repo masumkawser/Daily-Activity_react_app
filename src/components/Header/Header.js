7ca206c0884b7fb8ee09a29346fcd06129e7560d
masumkawser <kawser151722@gmail.com>
2022-09-29 14:36:14 +0600

    profile added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ActivityBody from '../ActivityBody/ActivityBody';
 import"./Header.css";
 const Header = () => {
     return (
         <div>
             <h1 className='Headertitle'>Daily Activity App</h1>
+            <ActivityBody></ActivityBody>
         </div>
     );
 };
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import"./Profile.css";
+const Profile = () => {
+    return (
+        <div>
+            <div className='profile'>
+            <h3>Md.Kawser ahamed</h3>
+            <p>web developer</p>
+            </div>
+            <div className='profile'>
+            <h3>Md.Kawser ahamed</h3>
+            <p>web developer</p>
+            </div>
+            <h3>Add Break</h3>
+            <div className='profile'>
+            <button>10 minit</button>
+            <button>15 minit</button>
+            <button>20 minit</button>
+            <button>25 minit</button>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
