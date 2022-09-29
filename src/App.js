028c3700f8f5375b26c02359007efc1169734f43
masumkawser <kawser151722@gmail.com>
2022-09-29 17:35:09 +0600

    updated data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-
 import React from 'react';
-import Header from './components/Header/Header';
-import Question from './components/Question/Question';
 
 const App = () => {
   return (
     <div>
-    <Header></Header>
-      <Question></Question>
+      <h1>kawser</h1>
+  
     </div>
   );
 };
